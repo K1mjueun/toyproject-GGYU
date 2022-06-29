32cb929bbc9aae3a9adef8c3a52068e763400b50
K1mjueun <jueun4779@gmail.com>
2022-06-29 17:49:24 +0900

    시작페이지에서 메인페이지로 넘어가기

--- GGYU.js
+++ GGYU.js
@@ -1,8 +1,35 @@
-// start-page
 
+// start-page
 const speechBubble = document.querySelector(".speech-bubble");
-console.log(speechBubble);
 
 setTimeout (function() {
   speechBubble.style.opacity = 1;
-},3000);
+},2000);
+
+const startP = document.getElementById("start-page");
+const mainP = document.getElementById("main-page");
+const hamonEarth = document.querySelector(".ham-on-earth");
+
+// 화면 전환 하기
+let displayAppear = [
+  {opacity: 0.8},
+  {opacity: 0.9},
+  {opacity: 1},
+];
+
+  let displayChangeTime = {
+    duration: 500,
+    iterations: 1
+  };
+
+  function displayChange(element, disappearTarget, appearTarget) {
+    element.addEventListener('click', function() {
+      disappearTarget.style.display = "none";
+      appearTarget.style.display = "flex";
+      appearTarget.animate(displayAppear,displayChangeTime);
+      appearTarget.style.opacity = 1;
+    });
+  };
+//
+
+  displayChange(hamonEarth,startP,mainP);
